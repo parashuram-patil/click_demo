28d08697b01bcd70d40585733adb23f55b15ca14
parashuram-patil <parasharam_patil@ymail.com>
2019-05-07 16:11:12 +0530

    show default values on --help

--- click_demo/click_demo.py
+++ click_demo/click_demo.py
@@ -7,14 +7,14 @@ def cli():
 
 
 @click.command()
-@click.option('--name', '-n', default='there', prompt='Please ent Name', help='Name of the person to greet')
+@click.option('--name', '-n', default='there', prompt='Please ent Name', help='Name of the person to greet', show_default=True)
 def hi(name):
     click.echo('Hi {}!'.format(name))    # used instead of print since print syntax differ by version, just a
     # safer side they say
 
 
 @click.command()
-@click.option('--name', '-n', default='there', prompt='Please ent Name', help='Name of the person to greet')
+@click.option('--name', '-n', default='there', prompt='Please ent Name', help='Name of the person to greet', show_default=True)
 def bye(name):
     click.echo('Bye {}!'.format(name))
 
