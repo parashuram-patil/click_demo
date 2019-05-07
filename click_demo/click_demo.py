2090a334b53c8431d990fb29effb400add78b046
parashuram-patil <parasharam_patil@ymail.com>
2019-05-07 16:15:58 +0530

    Grouping commands

--- click_demo/click_demo.py
+++ click_demo/click_demo.py
@@ -2,25 +2,27 @@ import click
 
 
 @click.group()
-def cli():
+def psp():
     pass
 
 
-@click.command()
-@click.option('--name', '-n', default='there', prompt='Please ent Name', help='Name of the person to greet', show_default=True)
+@psp.command()
+@click.option('--name', '-n', default='there', prompt='Please ent Name', help='Name of the person to greet',
+              show_default=True)
 def hi(name):
     click.echo('Hi {}!'.format(name))    # used instead of print since print syntax differ by version, just a
     # safer side they say
 
 
-@click.command()
-@click.option('--name', '-n', default='there', prompt='Please ent Name', help='Name of the person to greet', show_default=True)
+@psp.command()
+@click.option('--name', '-n', default='there', prompt='Please ent Name', help='Name of the person to greet',
+              show_default=True)
 def bye(name):
     click.echo('Bye {}!'.format(name))
 
 
-cli.add_command(hi)
-cli.add_command(bye)
+psp.add_command(hi)
+psp.add_command(bye)
 
 if __name__ == '__main__':
     pass
